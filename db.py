1f7e7f8d7c146e91d0546006e9d2ccd0dff737cf
Kevin Kim <58376469+kimanikevin254@users.noreply.github.com>
2023-11-14 09:49:56 +0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,29 @@
+import psycopg2
+import os
+
+def get_conn():
+    # create connection
+    conn = psycopg2.connect(os.environ.get("EXTERNAL_CONNECTION_STRING")
+
+          # Return connection to database
+          return conn
+
+def init_db():
+    # get connection
+    conn = get_conn()
+
+    # get cursor
+    cur = conn.cursor()
+
+    cur.execute("""
+        DROP TABLE IF EXISTS products;
+   	 
+    	  CREATE TABLE products (
+      	id INTEGER PRIMARY KEY,
+      	name TEXT UNIQUE NOT NULL,
+      	quantity INTEGER NOT NULL
+    	  );
+    """)
+    cur.close()
+    conn.commit()
+    conn.close()
