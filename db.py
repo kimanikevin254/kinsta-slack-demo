9075510c7e751b5e771958a38c55db0678454aa7
Kevin Kim <58376469+kimanikevin254@users.noreply.github.com>
2023-11-14 11:34:20 +0300

    Update db.py

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ def get_conn():
     # create connection
     conn = psycopg2.connect(os.environ.get("EXTERNAL_CONNECTION_STRING")
 
-          # Return connection to database
-          return conn
+    # Return connection to database
+    return conn
 
 def init_db():
     # get connection
